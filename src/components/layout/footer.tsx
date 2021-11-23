5628157f28fb40fa412b4ec6a4b10e6da9d38258
Scott <williamastatine@gmail.com>
2021-11-24 03:13:20 +0530

    updated

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,28 +1,28 @@
 import { MDBCol, MDBContainer, MDBRow } from 'mdbreact'
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import React from 'react'
 
 interface Props {
-    name: string;
+  name: string;
 }
 
 const PageFooter: React.FC<Props> = ({ name }) => {
-    return (
-        <React.Fragment>
-            <footer className="navbg">
-                <MDBContainer fluid>
-                    <MDBRow>
-                        <MDBCol className='justify-content-center text-center mt-4 pt-2 py-2' lg='12' sm='12'>
-                            <p className='font-weight-light text-small'>© Copyright
-                                <strong className='font-weight-bold'>  William Astatine </strong> {new Date().getFullYear()}<br />
-                                <small className='font-weight-light'>Desined by <NavLink to='/'>William Astatine</NavLink></small>
-                            </p>
-                        </MDBCol>
-                    </MDBRow>
-                </MDBContainer>
-            </footer>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <footer className="navbg">
+        <MDBContainer fluid>
+          <MDBRow>
+            <MDBCol className='justify-content-center text-center mt-4 pt-2 py-2' lg='12' sm='12'>
+              <p className='font-weight-light text-small'>© Copyright
+                <strong className='font-weight-bold'>  {name} Astatine </strong> {new Date().getFullYear()}<br />
+                <small className='font-weight-light'>Desined by <NavLink to='/'> {name} Astatine</NavLink></small>
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </footer>
+    </React.Fragment>
+  )
 }
 
 export default PageFooter
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,106 +1,106 @@
 import React, { Component } from "react";
 import {
-    MDBNavbar,
-    MDBNavbarBrand,
-    MDBNavbarNav,
-    MDBNavItem,
-    MDBNavLink,
-    MDBNavbarToggler,
-    MDBCollapse,
-    MDBIcon,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBIcon,
 } from "mdbreact";
 import { NavLink } from "react-router-dom";
 
 interface Props {
-    name: string;
+  name: string;
 }
 
 export default class AppNavbar extends Component<Props> {
-    state = {
-        isOpen: false,
-    };
+  state = {
+    isOpen: false,
+  };
 
-    toggleCollapse = () => {
-        this.setState({ isOpen: !this.state.isOpen });
-    };
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
-    navCollapse = () => {
-        this.setState({ isOpen: false })
-    }
+  navCollapse = () => {
+    this.setState({ isOpen: false })
+  }
 
-    render() {
-        return (
-            <MDBNavbar
-                color="navbg"
-                dark
-                expand="md"
-                fixed="top"
-                transparent={(this.state.isOpen === false) ? true : false}
-                scrolling
-            >
-                <MDBNavbarBrand>
-                    <NavLink className='navbar-brand' to="/">
-                        William 
-                    </NavLink>
-                </MDBNavbarBrand>
-                <MDBNavbarToggler onClick={this.toggleCollapse} />
-                <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-                    <MDBNavbarNav className="nav-pills nav-fill" right>
-                        <MDBNavItem>
-                            <MDBNavLink onClick={this.navCollapse} exact to="/">
-                                <MDBIcon fas icon="home" />Home
-                            </MDBNavLink>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <MDBNavLink onClick={this.navCollapse} to="/about">
-                                <MDBIcon fas icon="user-tie" />About
-                            </MDBNavLink>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <MDBNavLink onClick={this.navCollapse} to="/skills">
-                                <MDBIcon fas icon="tools" />Skills
-                            </MDBNavLink>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <MDBNavLink onClick={this.navCollapse} to="/contact">
-                                <MDBIcon fas icon="phone" />Contact
-                            </MDBNavLink>
-                        </MDBNavItem>
-                    </MDBNavbarNav>
-                    <MDBNavbarNav className="nav-fill" right>
-                        <MDBNavItem>
-                            <a className="nav-link waves-effect waves-light" href="https://www.twitter.com/WilliamAstatine"
-                                target="_blank" rel="noopener noreferrer">
-                                <MDBIcon fab icon="twitter" />{this.state.isOpen ? 'Twitter' : ''}
-                            </a>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <a className="nav-link waves-effect waves-light" href="https://www.facebook.com/WilliamAstatine"
-                                target="_blank" rel="noopener noreferrer">
-                                <MDBIcon fab icon="facebook" />{this.state.isOpen ? 'Facebook' : ''}
-                            </a>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <a className="nav-link waves-effect waves-light" href="https://www.instagram.com/williamastatine"
-                                target="_blank" rel="noopener noreferrer">
-                                <MDBIcon fab icon="instagram" />{this.state.isOpen ? 'Instagram' : ''}
-                            </a>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <a className="nav-link waves-effect waves-light" href="https://www.linkedin.com/in/williamastatine"
-                                target="_blank" rel="noopener noreferrer">
-                                <MDBIcon fab icon="linkedin" />{this.state.isOpen ? 'Linkedin' : ''}
-                            </a>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <a className="nav-link waves-effect waves-light" href="https://github.com/william-astatine"
-                                target="_blank" rel="noopener noreferrer">
-                                <MDBIcon fab icon="github" />{this.state.isOpen ? 'Github' : ''}
-                            </a>
-                        </MDBNavItem>
-                    </MDBNavbarNav>
-                </MDBCollapse>
-            </MDBNavbar>
-        );
-    }
+  render() {
+    return (
+      <MDBNavbar
+        color="navbg"
+        dark
+        expand="md"
+        fixed="top"
+        transparent={(this.state.isOpen === false) ? true : false}
+        scrolling
+      >
+        <MDBNavbarBrand>
+          <NavLink className='navbar-brand' to="/">
+            Scott
+          </NavLink>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav className="nav-pills nav-fill" right>
+            <MDBNavItem>
+              <MDBNavLink onClick={this.navCollapse} exact to="/">
+                <MDBIcon fas icon="home" />Home
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink onClick={this.navCollapse} to="/about">
+                <MDBIcon fas icon="user-tie" />About
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink onClick={this.navCollapse} to="/skills">
+                <MDBIcon fas icon="tools" />Skills
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink onClick={this.navCollapse} to="/contact">
+                <MDBIcon fas icon="phone" />Contact
+              </MDBNavLink>
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav className="nav-fill" right>
+            <MDBNavItem>
+              <a className="nav-link waves-effect waves-light" href="https://www.twitter.com/ScottAstatine"
+                target="_blank" rel="noopener noreferrer">
+                <MDBIcon fab icon="twitter" />{this.state.isOpen ? 'Twitter' : ''}
+              </a>
+            </MDBNavItem>
+            <MDBNavItem>
+              <a className="nav-link waves-effect waves-light" href="https://www.facebook.com/ScottAstatine"
+                target="_blank" rel="noopener noreferrer">
+                <MDBIcon fab icon="facebook" />{this.state.isOpen ? 'Facebook' : ''}
+              </a>
+            </MDBNavItem>
+            <MDBNavItem>
+              <a className="nav-link waves-effect waves-light" href="https://www.instagram.com/williamastatine"
+                target="_blank" rel="noopener noreferrer">
+                <MDBIcon fab icon="instagram" />{this.state.isOpen ? 'Instagram' : ''}
+              </a>
+            </MDBNavItem>
+            <MDBNavItem>
+              <a className="nav-link waves-effect waves-light" href="https://www.linkedin.com/in/williamastatine"
+                target="_blank" rel="noopener noreferrer">
+                <MDBIcon fab icon="linkedin" />{this.state.isOpen ? 'Linkedin' : ''}
+              </a>
+            </MDBNavItem>
+            <MDBNavItem>
+              <a className="nav-link waves-effect waves-light" href="https://github.com/william-astatine"
+                target="_blank" rel="noopener noreferrer">
+                <MDBIcon fab icon="github" />{this.state.isOpen ? 'Github' : ''}
+              </a>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    );
+  }
 }
